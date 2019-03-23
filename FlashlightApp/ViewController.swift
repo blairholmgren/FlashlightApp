412e838ff4048719e6122d4c019379003466dcfb
Blair Holmgren <blair@odin-mbp.local>
2019-03-22 18:46:48 -0700

    Adding other files

--- FlashlightApp/ViewController.swift
+++ FlashlightApp/ViewController.swift
@@ -9,12 +9,41 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let backgroundImageView = UIImageView()
 
+    @IBOutlet weak var helloWorldButton: UIButton!
+    
+    @IBOutlet weak var greenButton: UIButton!
+    @IBOutlet weak var redButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        setBackground()
+
+        helloWorldButton.setTitle("Flashlight", for: .normal)
+    }
+    
+    func setBackground() {
+        view.addSubview(backgroundImageView)
+        view.sendSubviewToBack(backgroundImageView)
+        backgroundImageView.translatesAutoresizingMaskIntoConstraints = false
+        backgroundImageView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        backgroundImageView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        backgroundImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        backgroundImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        
+        backgroundImageView.image = UIImage(named: "bg")
     }
 
-
+    @IBAction func flashLightButtonTapped(_ sender: Any) {
+        view.backgroundColor = .blue
+    }
+    
+    @IBAction func redButtonTapped(_ sender: Any) {
+        view.backgroundColor = .red
+    }
+    
+    @IBAction func greenButtonTapped(_ sender: Any) {view.backgroundColor = .green
+    }
 }
 
